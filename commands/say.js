f61845329011fd8ff535cee0cb1fd8dabf02d063
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-08-03 18:38:53 +0700

    Arguments

--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,12 @@
 module.exports = {
     name: 'say',
     execute: async(message, args) => {
+        let text = args.join(" ");
         if (!message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send("คุณไม่สามารถใช้คำสั่งนี้ได้")
         } else if (message.member.hasPermission("ADMINISTRATOR")) {
         message.delete()
-        await message.channel.send(args)
+        await message.channel.send(text)
         }
     }
 }
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.once('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const commandName = args.shift().toLowerCase();
 
         const command = client.commands.get(commandName)

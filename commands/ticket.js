9e90139eb8d67a0b110306b8e9b452d578409f32
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-08-03 18:06:33 +0700

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -4,7 +4,7 @@ const guild = new Discord.Guild()
 module.exports = {
     name: 'buy', 
     execute(message) {
-        guild.channels.create(`ticket-${message.author.username}`, {
+        guild.channels.create(`ticket`, {
             type: 'text',
             permissionOverwrites: [
                 {

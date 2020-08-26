9d2daa181de6c6304ad45fefccae3e5d2123dffc
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-08-26 20:04:59 +0700

    Update handsome.js

--- commands/handsome.js
+++ commands/handsome.js
@@ -9,6 +9,13 @@ module.exports = {
         .setTitle(`ความ หล่อ/สวย ของ ${message.author.username}`)
         .setDescription(`คุณมีความหล่อที่ 10000000% 💞`)
         message.channel.send(embed)
+        } else if(message.author.id === "694857558095298611") {
+            let hd = Math.floor(Math.random() * 100) + 1;
+            let embed = new Discord.MessageEmbed()
+            .setColor("PINK")
+            .setTitle(`ควาสวยของ ${message.author.username}`)
+            .setDescription(`คุณมีความสวยที่ 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000% 💞`)
+            message.channel.send(embed)
         } else {
             let hd = Math.floor(Math.random() * 100) + 1;
             let embed = new Discord.MessageEmbed()

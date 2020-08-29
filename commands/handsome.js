51f34cc65055d597b2cc2a38f5d1d799c3f7ff49
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-08-29 19:34:52 +0700

    เอออ

--- commands/handsome.js
+++ commands/handsome.js
@@ -1,8 +1,10 @@
 const MessageEmbed = require("discord.js")
 const Discord = require('discord.js')
+
 module.exports = {
     name: 'handsome',
     execute(message) {
+        let ping = message.mentions.users.first();
         if(message.author.id === "508473794994896896") {
         let embed = new Discord.MessageEmbed()
         .setColor("PINK")
@@ -16,11 +18,18 @@ module.exports = {
             .setTitle(`ควาสวยของ ${message.author.username}`)
             .setDescription(`คุณมีความสวยที่ 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000% 💞`)
             message.channel.send(embed)
-        } else {
+        } else if(!ping){
             let hd = Math.floor(Math.random() * 100) + 1;
             let embed = new Discord.MessageEmbed()
             .setColor("PINK")
             .setTitle(`ความ หล่อ/สวย ของ ${message.author.username}`)
+            .setDescription(`คุณมีความหล่อที่ ${hd}% 💞`)
+            message.channel.send(embed)
+        } else if(ping) {
+            let hd = Math.floor(Math.random() * 100) + 1;
+            let embed = new Discord.MessageEmbed()
+            .setColor("PINK")
+            .setTitle(`ความ หล่อ/สวย ของ ${ping.username}`)
             .setDescription(`คุณมีความหล่อที่ ${hd}% 💞`)
             message.channel.send(embed)
         }

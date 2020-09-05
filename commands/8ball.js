c37b3af37eb021da42e43ae10fde8865093d3326
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-09-05 13:44:05 +0700

    Easy

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,3 @@
-const MessageEmbed = require('discord.js')
-
 module.exports = {
     name: "8ball",
     execute(message, args) {
@@ -15,10 +13,6 @@ module.exports = {
         }
 
         if(!args) return;
-        let answer = new MessageEmbed()
-        .setTitle("8ball")
-        .setColor("GREEN")
-        .addField("This is your answer:", RANDOM())
-        message.channel.send(answer)
+        message.channel.send(RANDOM())
     }
 }

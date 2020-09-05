2dabb37037a22748328dbca311f36553c44e4057
NotVeryGoodAtAnything <64386361+NotVeryGoodAtAnything@users.noreply.github.com>
2020-09-05 13:56:50 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'say',
     args: true,
     execute: async(message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return;
         let text = args.join(" ");
         message.delete()
         await message.channel.send(text)

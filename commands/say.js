3a58b4f625ed0cfada3080447ae195f5c8183ee4
AyumiChan <68820538+Ayumi-Kaito@users.noreply.github.com>
2020-09-12 07:35:59 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,9 +2,12 @@ module.exports = {
     name: 'say',
     args: true,
     execute: async(message, args) => {
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) { 
+message.channel.send("Not Enough Permission")
+} else {
         let text = args.join(" ");
         message.delete()
         await message.channel.send(text)
+}
     }
 }
